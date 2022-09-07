623b6dec1eba852d57856c9f3f980612631a26ce
Bamboozled-Cat <rekjp89@hotmail.com>
2022-09-07 16:55:32 +0200

    fixed updated target calories again

--- routes/tracker.js
+++ routes/tracker.js
@@ -5,7 +5,7 @@ const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, trackerController.getTarget)
 
-router.put('/updateTarget', trackerController.updateTarget)
+router.post('/updateTarget', trackerController.updateTarget)
 // by Cy, added route to handle inserting a new food item to db
 router.post('/addFoodItem',trackerController.addFoodItem)
 
